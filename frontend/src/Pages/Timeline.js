6eea4fcfb12ed0d870802adf473f0596a9eca660
Matvey Bouhartsev <programmer@bouhartsev.top>
2021-12-04 16:51:04 +0300

    Add loader

--- frontend/src/Pages/Timeline.js
+++ frontend/src/Pages/Timeline.js
@@ -8,43 +8,57 @@ import {
     TimelineDot,
 } from '@mui/lab';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import CircularProgress from '@mui/material/CircularProgress';
 import TimelineItemStyled from '../components/TimelineItemStyled';
-// import { Button } from '@mui/material';
+import fetchData from '../utils/fetchData';
 
 const Timeline = () => {
-    const exampleArray = [
-        {
-            title: 'Example News Title',
-            date: '03.11.2021',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            source: 'https://example.com',
-        },
-        {
-            title: 'Example Title 2 very-very long, longer, the longest and etc. but asdasdfasd_;fljasdfvn.,mn213f.123!',
-            date: '10.10.2001',
-            text: 'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            source: 'https://example.com',
-            sourceName: 'Example Source',
-        },
-    ];
+    const [value, setValue] = React.useState();
+    const [loader, setLoader] = React.useState(true);
+
+    React.useEffect(() => {
+        fetchData('news/', setValue, setLoader); // ПОМЕНЯТЬ НА timeline/
+    }, []);
+    // const exampleArray = [
+    //     {
+    //         title: 'Example News Title',
+    //         date: '03.11.2021',
+    //         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         source: 'https://example.com',
+    //     },
+    //     {
+    //         title: 'Example Title 2 very-very long, longer, the longest and etc. but asdasdfasd_;fljasdfvn.,mn213f.123!',
+    //         date: '10.10.2001',
+    //         text: 'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         source: 'https://example.com',
+    //         sourceName: 'Example Source',
+    //     },
+    // ];
     return (
         <>
             <h2>Timeline</h2>
             <p>Text of description</p>
-            <LabTimeline className="timeline">
-                <TimelineItem>
-                    <TimelineSeparator>
-                        <TimelineDot variant="outlined">
-                            <ArrowUpwardIcon />
-                        </TimelineDot>
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent />
-                </TimelineItem>
-                {exampleArray.map((item) => {
-                    return <TimelineItemStyled item={item} />;
-                })}
-            </LabTimeline>
+
+            {loader ? (
+                <p>
+                    {value !== null ? <CircularProgress /> : <b>Error 404</b>}
+                </p>
+            ) : (
+                <LabTimeline className="timeline">
+                    <TimelineItem>
+                        <TimelineSeparator>
+                            <TimelineDot variant="outlined">
+                                <ArrowUpwardIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent />
+                    </TimelineItem>
+                    {value.map((item) => {
+                        return <TimelineItemStyled item={item} />;
+                    })}
+                </LabTimeline>
+            )}
         </>
     );
 };
