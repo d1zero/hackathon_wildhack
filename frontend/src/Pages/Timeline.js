71ac92a3098bb2d2f04ecde9ee5fd12297918d70
NickoLevin <niklev.02@mail.ru>
2021-12-04 06:53:25 +0500

    Timeline

--- frontend/src/Pages/Timeline.js
+++ frontend/src/Pages/Timeline.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Timeline as Timelline } from '@mui/lab';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { Button } from '@mui/material';
 
 const Timeline = () => {
     return (
         <div>
             <p>sdasdas</p>
+            <p>sdasdas</p>
+            <p>sdasdas</p>
+            <Timelline>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <Button>
+                            <TimelineDot />
+                        </Button>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>Eat</TimelineContent>
+                </TimelineItem>
+            </Timelline>
         </div>
     );
 };
